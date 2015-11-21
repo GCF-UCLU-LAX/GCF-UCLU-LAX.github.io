f3fb26dd9571120c443eb509fc6224118c58fd4d
Doroshenkot1510 <doroshenkot1510@gmail.com>
2015-11-21 15:20:24 +0000

    jQuery
    for ladies page

---
+++ js/ladies.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#first").mouseenter(function(){
+        alert("Welcome to Ladies Page!!!");
+    });
+});
